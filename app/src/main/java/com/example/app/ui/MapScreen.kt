f521a14c8f0530383feafc76ff84eb9118277371
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2023-06-23 09:28:48 +0200

    aggiunti i plugin per GoogleMaps API

--- app/src/main/java/com/example/app/ui/MapScreen.kt
+++ app/src/main/java/com/example/app/ui/MapScreen.kt
@@ -1,16 +1,37 @@
 package com.example.app.ui
 
-import android.content.Intent
-import android.net.Uri
-import android.util.Log
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.Modifier
+
 import com.example.app.viewModel.LocationViewModel
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerState
+import com.google.maps.android.compose.rememberCameraPositionState
+
 
 @Composable
 fun MapScreen(
     startLocationUpdate: ()->Unit,
-    locationViewModel: LocationViewModel,
+    locationViewModel: LocationViewModel
 ){
+
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
+    GoogleMap(
+        modifier = Modifier.fillMaxSize(),
+        cameraPositionState = cameraPositionState
+    ) {
+        Marker(
+            state = MarkerState(position = singapore),
+            title = "Singapore",
+            snippet = "Marker in Singapore"
+        )
+    }
 
 }
