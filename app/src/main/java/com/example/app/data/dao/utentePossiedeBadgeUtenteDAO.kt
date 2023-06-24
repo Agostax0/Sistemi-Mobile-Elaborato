67e3a2d96f491944ad81af9573def1e468ac14b4
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2023-06-24 17:34:00 +0200

    Roba per i badge Utente

--- app/src/main/java/com/example/app/data/dao/utentePossiedeBadgeUtenteDAO.kt
+++ app/src/main/java/com/example/app/data/dao/utentePossiedeBadgeUtenteDAO.kt
@@ -1,6 +1,7 @@
 package com.example.app.data.dao
 
 import androidx.room.*
+import com.example.app.data.relation.UtenteBadgeUtenteCrossRef
 import com.example.app.data.relation.UtentePossiedeBadgeUtente
 import kotlinx.coroutines.flow.Flow
 
@@ -10,6 +11,9 @@ interface UtentePossiedeBadgeUtenteDAO{
     @Transaction
     @Query("SELECT * FROM utente")
     fun getUtentiBadgeUtente() : Flow<List<UtentePossiedeBadgeUtente>>
+
+    @Query("SELECT * FROM UtenteBadgeUtenteCrossRef")
+    fun getBadgeUtentiRef(): Flow<List<UtenteBadgeUtenteCrossRef>>
     /*
     @Query("SELECT * FROM utente_possiede_badge_utente WHERE utente_possiede_badge_utente.ID = :userId")
     fun getBadgeUtenteObtainedOfUtente(userId:Int) : List<UtentePossiedeBadgeUtente>
--- app/src/main/java/com/example/app/data/repository/UtentePossiedeBadgeUtenteRepository.kt
+++ app/src/main/java/com/example/app/data/repository/UtentePossiedeBadgeUtenteRepository.kt
@@ -1,10 +1,13 @@
 package com.example.app.data.repository
 
 import com.example.app.data.dao.UtentePossiedeBadgeUtenteDAO
+import com.example.app.data.relation.UtenteBadgeUtenteCrossRef
 import com.example.app.data.relation.UtentePossiedeBadgeUtente
 import kotlinx.coroutines.flow.Flow
 
 class UtentePossiedeBadgeUtenteRepository(private val utentePossiedeBadgeUtenteDAO: UtentePossiedeBadgeUtenteDAO) {
 
     val utentiBadgeUtente: Flow<List<UtentePossiedeBadgeUtente>> = utentePossiedeBadgeUtenteDAO.getUtentiBadgeUtente()
+
+    val utenteBadgeUtenteRef: Flow<List<UtenteBadgeUtenteCrossRef>> = utentePossiedeBadgeUtenteDAO.getBadgeUtentiRef()
 }
